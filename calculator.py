f9b9b395e9f3963f341dc6196d747012c72a6e38
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 15:40:12 -0700

    initial commit

---
+++ calculator.py
@@ -0,0 +1,11 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
