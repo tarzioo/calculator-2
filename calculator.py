9b3ee65d98b4363bd478bc6025fcc5e67b47d105
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 15:57:58 -0700

    implemented add, subtract,  and multiply functions

--- calculator.py
+++ calculator.py
@@ -7,5 +7,21 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
+while True:
+    expression = raw_input("Enter expression here: ")
+    tokens = expression.split(" ")
+    if tokens[0] == 'q':
+        break
 
+    integer_one = int(tokens[1])
+    integer_two = int(tokens[2])
+
+    if tokens[0] == '+':
+        print add(integer_one, integer_two)
+    elif tokens[0] == '-':
+        print subtract(integer_one, integer_two)
+    elif tokens[0] == '*':
+        print multiply(integer_one, integer_two)
+    else:
+        break       
 # Your code goes here
